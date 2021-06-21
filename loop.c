fd5f90c3f43f7915734e0999518133415d0e6e87
Raquib Khan <khanraquib01@gmail.com>
2021-06-21 18:00:57 +0530

    initial commit

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+/*
+Types of Loops:
+1. For loop:
+	properties:
+	1. can have more than 1 initialization like for(i=0;j=0;i<5;i++)
+	2. initialization is optional like for(;i<5;i++)
+	3. conditional part is also optional like for(i=0;;i++)
+	4. can have more than one updation and it is also optional
+2. Do while loop
+3. while loop
+*/
+}
