d7c655f47e62c97a80619d5e0ec6b9206ff765d5
Raquib Khan <khanraquib01@gmail.com>
2021-06-24 12:05:51 +0530

    learned arrays and  pointer

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int fibonacci_recursive(int n){
+	if(n==1 || n==2){
+		return n-1;
+	}
+	return fibonacci_recursive(n-1)+fibonacci_recursive(n-2);
+}
+
+int fibonacci_iterative(int n){
+	int f1 = 0;
+	int f2 = 1;
+	for(int i=0;i<n-2;i++){
+		f2 = f1 + f2;
+		f1 = f2 -f1;
+	}
+	return f2;
+}
+
+
+int main(void){
+	int n;
+	printf("Enter the value of n\t");
+	scanf("%d",&n);
+	printf("Using Iterative %d\n",fibonacci_iterative(n));
+	printf("Using Recursion %d\n",fibonacci_recursive(n));
+}
